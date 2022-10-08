ad16dc4cd39d04457ff62bdafcf9da5d6cfcdfd1
Fernanda Palacios <fernandapalacios.dev@gmail.com>
2022-10-07 22:43:02 -0500

    handle Errors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose'
 import express from 'express'
 import * as dotenv from 'dotenv'
+import { StatusHttp } from './errorCustom.js'
 
 import {Koder} from './models/koders.model.js'
 
@@ -17,16 +18,88 @@ server.use(express.json())
 
 // Routers
 
+// query params
+
 
 server.get('/koders', async (request, response) => {
-    const allKoders = await Koder.find({})
-    response.json({
-        success: true,
-        data: {
-            koders: allKoders
+    try {
+        const {gender, age} = request.query
+
+        const filters = {}
+        
+        if(gender) {
+            filters.gender = gender
         }
-    })
+
+        if(age) filters.age = age 
+
+        console.log(filters)
+        
+        const allKoders = await Koder.find(filters)
+        response.json({
+            success: true,
+            data: {
+                koders: allKoders
+            }
+        })
+    } catch (error) {
+        response.status(400).json({
+            success: false,
+            message: error.message
+        })
+    }
 })
+
+
+
+// POST /koders
+server.post('/koders', async (request, response) => {
+    try {
+        const {body: newKoder} = request
+        const koderCreated = await Koder.create(newKoder)
+
+        response.json({
+            success: true,
+            message: 'Koder creado'
+        })
+    } catch (error) {
+        response.status(400).json({
+            success: false,
+            message: error.message
+        })
+    }
+})
+
+
+// GET /koders/:id
+
+server.get('/koders/:id', async (request, response) => {
+    try {
+        const {id} = request.params
+        const koderFound = await Koder.findById(id)
+        console.log(koderFound)
+
+        // Lanzar un Error  - throw
+        if(!koderFound) throw new StatusHttp('Koder no encontrado D:', 404)
+        
+        response.json({
+            success: true,
+            data: {
+                koder: koderFound
+            }
+        })
+    } catch (error) {
+        response.status(error.status).json({
+            success: false,
+            message: error.message
+        })
+    }
+    
+})
+
+// try catch
+
+// throw
 
 
 /*
